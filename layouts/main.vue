41c3132f9a94640b283150403c4066450c12d2c6
Дмитрий Паршаков <knutdmit@yandex.ru>
2019-07-21 22:57:53 +0500

    component header

--- layouts/main.vue
+++ layouts/main.vue
@@ -1,16 +1,17 @@
 <template>
   <div>
-    header
+    <app-header />
     <nuxt />
     <app-footer />
   </div>
 </template>
 
 <script>
+  import AppHeader from '@/components/app/AppHeader'
   import AppFooter from '@/components/app/AppFooter'
   export  default {
     components: {
-      AppFooter
+      AppHeader, AppFooter
     }
   }
 </script>
