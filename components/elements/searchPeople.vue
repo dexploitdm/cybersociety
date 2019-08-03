e30d6b29d8dc2dde27b8890af82a64cdb6078f3c
Дмитрий Паршаков <knutdmit@yandex.ru>
2019-08-03 15:11:48 +0500

    block search

--- components/elements/searchPeople.vue
+++ components/elements/searchPeople.vue
@@ -1,7 +1,45 @@
 <template>
-    <div class="search-people">
-      searchPeople
-    </div>
+    <section class="search-people lg">
+      <div class="search-layout">
+        <div class="row">
+          <div class="search-header">
+            <div class="search-header__title">
+              Interested?
+            </div>
+            <div class="search-header__desc">
+              Here are two ways you can get started with SimilarWeb Today
+            </div>
+          </div>
+          <div class="search-content">
+              <a class="search-content-box">
+                <div class="icon">
+                  <img src="/images/icons/globus.png">
+                </div>
+                <div class="button">
+                  Try SimilarWeb
+                </div>
+                <div class="search-text">
+                  Gain hands-on experience with your own free SimilarWeb Account
+                </div>
+              </a>
+              <a class="search-content-box two">
+                <div class="icon">
+                  <img src="/images/icons/brain.png">
+                </div>
+                <div class="button">
+                  Try SimilarWeb
+                </div>
+                <div class="search-text">
+                  Gain hands-on experience with your own free SimilarWeb Account
+                </div>
+              </a>
+          </div>
+
+
+
+        </div>
+      </div>
+    </section>
 </template>
 
 <script>
